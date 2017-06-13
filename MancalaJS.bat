56d0c6b51eeaf06758cf64422d746585537f59bf
Roel Janszen <rjanszen@sogyo.nl>
2017-06-13 10:37:24 +0200

    Added comments to the .bat file

--- MancalaJS.bat
+++ MancalaJS.bat
@@ -1,3 +1,4 @@
+// This .bat file will make it easy to launch the JavaScript file by starting node in a command prompt
 @echo off			
 title NodeJS hook - Mancala	// Title
 node %~dp0MancalaJS.js		// '%~dp0' current path + 'jscript.js'
